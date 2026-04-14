2cd75d094a2e37376872892eac3e4f9ce2da2787
betrguy <albert.eustace97@gmail.com>
2026-04-14 16:07:16 -0400

    Quartz sync: Apr 14, 2026, 4:07 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -9,7 +9,6 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/betrguy",
-      "Quartz Discord": "https://discord.gg/cRFFHYye7t",
     },
   }),
 }
